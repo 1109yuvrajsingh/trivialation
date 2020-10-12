f68219b7246cd96c4a6d1ee2814d9e89979fe666
= <=>
2020-10-12 09:31:34 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/teams/codehard/trivialation/MainActivity.kt
@@ -0,0 +1,12 @@
+package teams.codehard.trivialation
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
